ac803f3f1c54bdf192be53f77bd72e69cc1949b6
Somdip Dey <somdip007@hotmail.com>
2017-10-24 15:23:33 +0100

    Initial check-in of module The_Even_Fibonacci_Sum_Calculator

---
+++ The_Even_Fibonacci_Sum_Calculator/FibonacciList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace The_Even_Fibonacci_Sum_Calculator
+{
+    public class FibonacciList
+    {
+        public List<int> fibonacciSeriesForN = new List<int>();
+
+        public FibonacciList(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                fibonacciSeriesForN.Add(fillTheSeries(i));
+            }
+        }
+
+        private int fillTheSeries(int n){
+            if (n == 0 || n == 1) 
+                return n;
+            else
+                return fillTheSeries(n - 1) + fillTheSeries(n - 2);
+        }
+    }
+}
---
+++ The_Even_Fibonacci_Sum_Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace The_Even_Fibonacci_Sum_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var readN = Console.ReadLine();
+            FibonacciList fibonacciSeries = new FibonacciList(int.Parse(readN));
+            var summation = 0;
+            foreach (int item in fibonacciSeries.fibonacciSeriesForN)
+            {
+                if(item != 0 || item != 1)
+                {
+                    if (item % 2 == 0)
+                        summation += item;
+                }
+            }
+            Console.WriteLine("The Even Fibonacci Sum for " + readN + " is "+ summation);
+            Console.WriteLine("Press enter or any key to exit this program");
+            Console.ReadLine();
+        }
+    }
+}
